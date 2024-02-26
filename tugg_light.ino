f09cb9d8ccd4e562c6558296b7c6e4dd97be6301
erimli <31303451+erimli@users.noreply.github.com>
2024-02-25 19:23:34 -0800

    All the sounds and headlights coded

--- tugg_light.ino
+++ tugg_light.ino
@@ -5,28 +5,35 @@
 SerialMP3Player mp3(RX,TX);
 int plcount = -1;
 
-#define PWMRIGHTVERTICAL 2
+#define PWMRIGHTVERTICAL   2 
 #define PWMRIGHTHORIZONTAL 3
-#define PWMRIGHTSWITCH 4
+#define PWMRIGHTSWITCH     4
+
+#define HEADLIGHTS    5
+#define BLINKER_LEFT  6
+#define BLINKER_RIGHT 7
 
 int duration_vertical; // PWM pulse width variable
 int duration_horizontal; // PWM pulse width variable
 int duration_switch; // PWM pulse width variable
 
-#define HORIZONTAL_HIGH_THRESHOLD   1600
+#define HORIZONTAL_HIGH_THRESHOLD   1650
 #define HORIZONTAL_MIDDLE_THRESHOLD 1400
-#define HORIZONTAL_LOW_THRESHOLD    1100
-#define VERTICAL_HIGH_THRESHOLD     1600
+#define HORIZONTAL_LOW_THRESHOLD    1280
+#define VERTICAL_HIGH_THRESHOLD     1650
 #define VERTICAL_MIDDLE_THRESHOLD   1400
-#define VERTICAL_LOW_THRESHOLD      1100
-#define SWITCH_HIGH_THRESHOLD       1600
+#define VERTICAL_LOW_THRESHOLD      1280
+#define SWITCH_HIGH_THRESHOLD       1650
 #define SWITCH_MIDDLE_THRESHOLD     1400
-#define SWITCH_LOW_THRESHOLD        1100
+#define SWITCH_LOW_THRESHOLD        1280
 
 #include <Adafruit_NeoPixel.h>
-#define LED_PIN    6
+#define LED_PIN   6
 #define LED_COUNT 8
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+void playthesong(int this_song=2, int this_duration=2000, int this_volume=50);
+enum SongList { StartupEngine = 03, GetOutOfTheWay = 07, HappyDance = 05, MotorSound = 02, BreakingDown = 04, Horn = 01, Alarm = 10, DanceMusic = 06, NoSong=00 };
 
 void setup() {
 Serial.begin(9600);     // start serial interface
@@ -36,25 +43,32 @@ delay(500);             // wait for init
 mp3.sendCommand(CMD_SEL_DEV, 0, 2);   //select sd-card
 delay(500);             // wait for init
 
+pinMode(HEADLIGHTS, OUTPUT);
+pinMode(BLINKER_LEFT, OUTPUT);
+pinMode(BLINKER_RIGHT, OUTPUT);
+
 strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
 strip.show();            // Turn OFF all pixels ASA
 strip.setBrightness(75); // Set BRIGHTNESS to about 1/5 (max = 255)
 
+//Light Test
+rainbow(1);
 
 //Play Startup Sound
-mp3.play(03,100); 
-delay(7000);
+//mp3.play(03,100); 
+//delay(7000);
 mp3.stop(); 
 
-//Light Test
-rainbow(10);
+digitalWrite(HEADLIGHTS, HIGH);
+playthesong(StartupEngine, 7000, 50); 
+
 
 }
+
 
 // the loop function runs over and over again forever
 void loop() {
 
-  enum SongList { StartupEngine = 03, GetOutOfTheWay = 07, HappyDance = 05, MotorSound = 02, BreakingDown = 04, Horn = 01, Alarm = 10, DanceMusic = 06 };
   SongList play_song;
 
   duration_vertical   = pulseIn(PWMRIGHTVERTICAL, HIGH); // capture pwm input
@@ -63,30 +77,51 @@ void loop() {
 
 
 // PWM frequency to sound mapping
-  if ((duration_vertical > VERTICAL_HIGH_THRESHOLD) && (duration_horizontal < HORIZONTAL_LOW_THRESHOLD)) { play_song = SongList::StartupEngine;}
+  if ((duration_vertical > VERTICAL_HIGH_THRESHOLD) && (duration_horizontal < HORIZONTAL_LOW_THRESHOLD)) { play_song = SongList::NoSong;}
   else if ((duration_vertical > VERTICAL_HIGH_THRESHOLD) && (duration_horizontal > HORIZONTAL_HIGH_THRESHOLD)) { play_song = SongList::GetOutOfTheWay;}
   else if ((duration_vertical < VERTICAL_LOW_THRESHOLD) && (duration_horizontal > HORIZONTAL_HIGH_THRESHOLD)) { play_song = SongList::HappyDance;}
-  else if ((duration_vertical < VERTICAL_LOW_THRESHOLD) && (duration_horizontal < HORIZONTAL_LOW_THRESHOLD)) { play_song = SongList::MotorSound;}
+  else if ((duration_vertical < VERTICAL_LOW_THRESHOLD) && (duration_horizontal < HORIZONTAL_LOW_THRESHOLD)) { play_song = SongList::StartupEngine;}
   else if (duration_vertical > VERTICAL_HIGH_THRESHOLD) { play_song = SongList::BreakingDown;}
   else if (duration_vertical < VERTICAL_LOW_THRESHOLD) { play_song = SongList::Horn;}
   else if (duration_vertical > HORIZONTAL_HIGH_THRESHOLD) { play_song = SongList::Alarm;}
   else if (duration_vertical < HORIZONTAL_LOW_THRESHOLD) { play_song = SongList::DanceMusic;}
   else play_song = SongList::MotorSound;
 
-playthesong(StartupEngine);
+// playthesong(StartupEngine,7000,50);
   switch (play_song) {
-    case SongList::StartupEngine:
+    case SongList::NoSong:
+      mp3.stop(); 
       //playthesong(StartupEngine, 7000, 50); 
     break;
-
-
+    case SongList::GetOutOfTheWay:
+      playthesong(GetOutOfTheWay, 2000, 50); 
+    break;
+    case SongList::HappyDance:
+      playthesong(HappyDance, 7000, 50); 
+    break;
+    case SongList::StartupEngine:
+      playthesong(StartupEngine, 7000, 50); 
+    break;
+    case SongList::BreakingDown:
+      playthesong(BreakingDown, 7000, 50); 
+    break;
+    case SongList::Horn:
+      playthesong(Horn, 7000, 50); 
+    break;
+    case SongList::Alarm:
+      playthesong(Alarm, 7000, 50); 
+    break;
+    case SongList::DanceMusic:
+      playthesong(DanceMusic, 7000, 50); 
+    break;
     default:
-    // statements
+      playthesong(MotorSound,10900,50);
+
     break;
   }
 
 
-  rainbow (10000);
+//  rainbow (1);
 
 }
 void rainbow(int wait) {
@@ -97,12 +132,12 @@ void rainbow(int wait) {
   }
 }
 
-void playthesong(int this_song){
+void playthesong(int this_song, int this_duration, int this_volume){
   //void playthesong(int this_song, int duration=2000, int volume=50){
 
-  mp3.play(this_song); 
-  delay(4000);
-  mp3.stop();  
+  mp3.play(this_song, this_volume); 
+  delay(this_duration);
+ // mp3.stop();  
 }
 void playhorn(){
   mp3.play(4,100); 
