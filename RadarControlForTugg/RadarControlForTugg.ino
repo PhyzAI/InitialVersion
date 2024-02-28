f35babe167c0b0ac8146d473998cb6d9f0e161aa
PhyzAI <155505908+PhyzAI@users.noreply.github.com>
2024-02-27 19:31:08 -0800

    Even More minor changes :halo:

--- RadarControlForTugg/RadarControlForTugg.ino
+++ RadarControlForTugg/RadarControlForTugg.ino
@@ -1,24 +1,15 @@
-int count = 0;
-int i = 0;
-void setup() {
-  // put your setup code here, to run once:
+#include <Servo.h>
 
+Servo myservo;  // create servo object to control a servo
+int pos = 0;    // variable to store the servo position
+
+void setup() {
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-  count = random(1, 50);
-  i = 0;
-
-  while (i < count){
-
-    tone(10, random(30, 4978), 250);
-    delay(random(20, 100));
-    i = i + 1;
-
-  }
-  
-  delay(random(0, 15000));
-
+  delay(random(15000, 90000));
+  myservo.write(random(10,170));              
+  delay(random(500, 3000));        
+  myservo.write(90); 
 }
