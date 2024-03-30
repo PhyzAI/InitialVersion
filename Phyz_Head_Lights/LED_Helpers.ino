f8990fbc61517bc35a972045210c87f9461c1c23
Alexandra Steiner <asteine8@gmail.com>
2024-03-30 12:18:19 -0700

    Update LED_Helpers.ino

--- Phyz_Head_Lights/LED_Helpers.ino
+++ Phyz_Head_Lights/LED_Helpers.ino
@@ -3,7 +3,7 @@
 /** @brief Sets entire strip to specified color value and updates
  *  @param strip The strip to change color on
  *  @param color uint_32 color value to update color to
- *  @return Void.
+ *  @return Void
  */
 void setStripToColor(Adafruit_NeoPixel& strip, uint32_t color) {
     for (int n = 0; n < strip.numPixels(); n++) {
@@ -12,12 +12,12 @@ void setStripToColor(Adafruit_NeoPixel& strip, uint32_t color) {
     strip.show();
 }
 
-/** @brief Sets entire strip to specified color value and updates
+/** @brief Sets entire strip to specified color value and updatess
  *  @param strip The strip to change color on
  *  @param r color red value
  *  @param g color red value
  *  @param b color red value
- *  @return Void.
+ *  @return Void
  */
 void setStripToColor(Adafruit_NeoPixel& strip, uint8_t r, uint8_t g, uint8_t b) {
     for (int n = 0; n < strip.numPixels(); n++) {
@@ -31,8 +31,12 @@ class LedStripLightShow {
         Adafruit_NeoPixel& strip;
         unsigned long millisAtStart;
         int millisToDelay;
-        enum ShowState {NONE = 0, theaterChase = 1, }
+
     public:
+        enum ShowState {NONE = 0, THEATER_CHASE = 1, RAINBOW_THEATER_CHASE = 2, RAINBOW = 3, SOLID = 4};
+        ShowState currentState = NONE;
+        uint32_t color = 0xFF0000; // Red by default
+
         LedStripLightShow(Adafruit_NeoPixel& s) {
             strip = s;
             millisAtStart = millis();
@@ -45,7 +49,73 @@ class LedStripLightShow {
             millisToDelay = delay;
         }
 
+        // Call me to start a new type of show
+        void setShow(int showState) {
+            currentState = showState; // Update which show we are playing rn
+
+            millisAtStart = millis();
+
+            // Initial Setup for a show
+            switch (currentState) {
+                // case THEATER_CHASE:
+                //     // generate
+                //     // output led
+                //     break;
+                case RAINBOW_THEATER_CHASE:
+                case RAINBOW:
+                    color = 0xFF0000; // Start the rainbow at red
+                    break;
+
+            }
+        }
+
+        // function to check for what type of show needed
+        // if (function = rainbow) --> state = whatever number rainbow is
+        
+        // Updates this strand
+        uint8_t updateLEDs() {
+            
+            switch (currentState) {
+                case NONE:
+                    setStripToColor(strip, 0x000000);
+                    break;
+                case THEATER_CHASE:
+                    // generate
+                    // output led
+                    break;
+                case RAINBOW_THEATER_CHASE:
+                    // generate
+                    // output led
+                    break;
+                case RAINBOW: // Generates a solid rainbow on all leds in the strip
+                    for (int i = 0; i < strip.numPixels(); i++) {
+                        strip.getPixelColor(i)
+                    }
+                    // output led
+                    break;
+                case SOLID: // Applies color to entire strip
+                    setStripToColor(strip, color);
+                    // output led
+                    break;
+            }
+            
+            strip.show();
+        }
+};
 
 
-}
 
+// void I2Ccommand (int command){
+//  switch (command){
+//   case x:
+//   // do x command
+//   break;
+//   case x:
+//   // do x command
+//   break;
+//   case x:
+//   // do x command
+//   break;
+//  } 
+//  }
+// }
